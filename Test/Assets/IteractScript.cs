be6aaf3c63c5c2578a9a40118c8d0146cbed9643
Davit Harutyunyan <davit.harutyunyan@arm.synisys.com>
2016-05-16 21:15:52 +0400

    Door open close script implemented.

---
+++ Test/Assets/IteractScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IteractScript : MonoBehaviour
+{
+
+    public float distance = 5f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log("Mouse Clicked");
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, distance))
+            {
+                Debug.Log("Ray Sent");
+                if (hit.collider.CompareTag("Door"))
+                {
+                    hit.collider.gameObject.GetComponent<OpenDoor>().ChangeDoorState();
+                }
+            }
+        }
+
+    }
+}
---
+++ Test/Assets/OpenDoor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenDoor : MonoBehaviour
+{
+    public bool open = true;
+    public float openAngle = 90f;
+    public float closeAngle = 0f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+    public void ChangeDoorState()
+    {
+        open = !open;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (open)
+        {
+            Quaternion targetRotation = Quaternion.Euler(0, openAngle, 0);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, 2f * Time.deltaTime);
+            //Debug.Log("open"+transform.localRotation);
+        }
+        else
+        {
+            Quaternion targetRotation2 = Quaternion.Euler(0, closeAngle, 0);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation2, 2f * Time.deltaTime);
+        }
+    }
+}

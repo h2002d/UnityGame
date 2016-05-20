13bfb2d73507bfa9711da0820e4d556eef3ecb1a
Davit Harutyunyan <davit.harutyunyan@arm.synisys.com>
2016-05-20 21:24:09 +0400

    Start screen added.

---
+++ Test/Assets/CloseScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloseScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Application.Quit();
+
+        }
+       
+    }
+}
